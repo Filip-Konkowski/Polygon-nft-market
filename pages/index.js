33c4b444604eeb9d7eb7cd0620973816f4943973
Filip Konkowski <fkonkowski@gmail.com>
2022-01-15 13:00:41 +0000

    Fetch NFTs from blockchain

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import {
 
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
+import { ethers } from 'ethers'
 
 export default function Home() {
   const [nfts, setNfts] = useState([])
@@ -19,13 +20,82 @@ export default function Home() {
   }, [])
 
   // this is loading for us all states of NFTs in react world we need useEffect() 
-  async function loadNFTs() {}
+  async function loadNFTs() {
+    const provider = new ethers.providers.JsonRpcProvider()
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+
+    const data = await marketContract.fetchMarketItems()
+
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenURI(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+
+      return item;
+    }))
+
+    setNfts(items)
+    setLoadingState('loaded')
+  }
+
+  async function buyNFT(nft) {
+    const web3Model = new Web3Modal()
+    const connection = await web3Model.connection()
+    const provider = new ethers.providers.Web3Provider(connection)
+
+    const signer = provider.getSigner()
+    const contract =  new ethers.Contract(nftmarketaddress, Market.abi, signer)
+
+    const price = ethers.utils.parseUnits(nft.price.toString, 'ether')
+    const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, {
+      value: price
+    })
+    //wait until transaction executed in blockchain
+    await transaction.wait()
+    // load new state of the nft (should be one less)
+    loadNFTs()
+  }
+
+  if (loadingState === 'loaded' && !nfts.lenght) return (
+      <h1 className="px-20 py-20 text-3xl">No items in marketplace</h1>
+  )
 
 
   return (
-    <div className={styles.container}>
-     
-      <h1>Home</h1>
+    <div className='flex justify-center'>
+      <div className='px-4' style={{ maxWidth: '1600px'}}>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+          {
+            nfts.map((nft, i) => {
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image} />
+                <div className="p-4">
+                  <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
+                  <div style={{ height: '70px', overflow: 'hidden' }}>
+                    <p className="text-gray-400">{nft.description}</p>
+                  </div>
+                </div>
+                <div className="p-4 bg-black">
+                  <p className="text-2xl mb-4 font-bold text-white">{nft.price} ETH</p>
+                  <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                </div>
+              </div>
+            })
+          }
+        </div>
+      </div>
     </div>
+
   )
 }
